0d6ca448633a09075cfea7a3883acee891f78957
Jerry Li <jerryyli12@gmail.com>
2021-02-16 09:29:21 -0500

    fixed

--- extract.py
+++ extract.py
@@ -31,11 +31,11 @@ class element_has_options(object):
 driver = webdriver.Chrome()
 driver.get('https://msc.fema.gov/portal/advanceSearch')
 
-outp = csv.writer(open('file_to_date.csv','w', newline=''), delimiter=',')
+outp = csv.writer(open('file_to_date.csv','a', newline=''), delimiter=',')
 
 try:
     # Wait until website is loaded
-    _ = WebDriverWait(driver, 30).until(
+    _ = WebDriverWait(driver, 120).until(
         EC.presence_of_element_located((By.ID, 'selstate'))
     )
 
@@ -44,7 +44,7 @@ try:
     select_state.select_by_visible_text('FLORIDA')
 
     # Wait until county dropdown is loaded
-    _ = WebDriverWait(driver, 30).until(
+    _ = WebDriverWait(driver, 120).until(
         element_has_options((By.ID, 'selcounty'))
     )
 
@@ -54,12 +54,14 @@ try:
         if county_option.get_attribute('value') != 'none':
             # Select county and wait until community dropdown is loaded
             county_option.click()
-            _ = WebDriverWait(driver, 30).until(
+            _ = WebDriverWait(driver, 120).until(
                 element_has_options((By.ID, 'selcommunity'))
             )
 
             county_name_lower = county_option.get_attribute('text').lower()
             outp_path = 'output/' + county_name_lower
+            if os.path.isdir(outp_path):
+                continue
             os.makedirs(outp_path, exist_ok=True)
 
             # Select ALL JURISDICTIONS community
@@ -71,110 +73,122 @@ try:
 
             # Click search and wait until search results are loaded
             driver.find_element_by_id('mainSearch').click()
-            _ = WebDriverWait(driver, 30).until(
+            _ = WebDriverWait(driver, 120).until(
                 EC.visibility_of(driver.find_element_by_class_name('searchresults'))
             )
 
             # Open up the table
-            driver.find_element_by_id('eff_root').click()
-            _ = WebDriverWait(driver, 30).until(
-                EC.element_to_be_clickable((By.ID, 'eff_lomc_root'))
-            )
-            driver.find_element_by_id('eff_lomc_root').click()
-            _ = WebDriverWait(driver, 30).until(
-                EC.element_to_be_clickable((By.ID, 'eff_main_loma_root'))
-            )
-            driver.find_element_by_id('eff_main_loma_root').click()
-            eff_loma_root_elt = driver.find_element_by_css_selector('div[aria-labelledby="eff_main_loma_root"]')
-            _ = WebDriverWait(driver, 30).until(
-                EC.visibility_of(eff_loma_root_elt)
-            )
-            prev_header = ''
+            table_loaded = True
+            try:
+                driver.find_element_by_id('eff_root').click()
+                _ = WebDriverWait(driver, 120).until(
+                    EC.element_to_be_clickable((By.ID, 'eff_lomc_root'))
+                )
+                driver.find_element_by_id('eff_lomc_root').click()
+                _ = WebDriverWait(driver, 120).until(
+                    EC.element_to_be_clickable((By.ID, 'eff_main_loma_root'))
+                )
+                driver.find_element_by_id('eff_main_loma_root').click()
+                eff_loma_root_elt = driver.find_element_by_css_selector('div[aria-labelledby="eff_main_loma_root"]')
+                _ = WebDriverWait(driver, 120).until(
+                    EC.visibility_of(eff_loma_root_elt)
+                )
+            except Exception as e:
+                table_loaded = False
 
-            print(eff_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML'))
+            if table_loaded:
+                prev_header = ''
 
-            while True:
-                eff_loma_list_elt = eff_loma_root_elt.find_element_by_id('eff_loma_list')
-                eff_loma_rows = eff_loma_list_elt.find_elements_by_css_selector('tr[role="row"]')
+                print(eff_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML'))
 
-                # Loop through all rows in table
-                for row in eff_loma_rows:
-                    entries = row.find_elements_by_css_selector('td')
-                    file_id = entries[0].get_attribute('innerHTML')
-                    file_date = entries[1].get_attribute('innerHTML')
-                    file_link = entries[2].find_element_by_css_selector('a').get_attribute('href')
+                while True:
+                    eff_loma_list_elt = eff_loma_root_elt.find_element_by_id('eff_loma_list')
+                    eff_loma_rows = eff_loma_list_elt.find_elements_by_css_selector('tr[role="row"]')
 
-                    act_link = 'https://map1.msc.fema.gov/data' + file_link[file_link.find('filepath') + 9 : file_link.find('.pdf') + 4]
-                    # print(file_id, file_date, file_link)
+                    # Loop through all rows in table
+                    for row in eff_loma_rows:
+                        entries = row.find_elements_by_css_selector('td')
+                        file_id = entries[0].get_attribute('innerHTML')
+                        file_date = entries[1].get_attribute('innerHTML')
+                        file_link = entries[2].find_element_by_css_selector('a').get_attribute('href')
 
-                    # Download and write to csv
-                    if int(file_date[-4:]) >= 2000:
-                        urlretrieve(act_link, outp_path + '/' + file_id + '.pdf')
-                        outp.writerow([file_id, file_date])
+                        act_link = 'https://map1.msc.fema.gov/data' + file_link[file_link.find('filepath') + 9 : file_link.find('.pdf') + 4]
+                        # print(file_id, file_date, file_link)
 
-                try:
-                    # Click next to go to next page
-                    next_button = eff_loma_root_elt.find_element_by_css_selector('.next:not(.disabled)')
-                    next_button.click()
-                    _ = WebDriverWait(driver, 30).until(
-                        lambda dr: eff_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML') != prev_header
-                    )
-                    prev_header = eff_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML')
-                    print(prev_header)
+                        # Download and write to csv
+                        if int(file_date[-4:]) >= 2000:
+                            urlretrieve(act_link, outp_path + '/' + file_id + '.pdf')
+                            outp.writerow([file_id, file_date])
 
-                except NoSuchElementException as e:
-                    break
+                    try:
+                        # Click next to go to next page
+                        next_button = eff_loma_root_elt.find_element_by_css_selector('.next:not(.disabled)')
+                        next_button.click()
+                        _ = WebDriverWait(driver, 120).until(
+                            lambda dr: eff_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML') != prev_header
+                        )
+                        prev_header = eff_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML')
+                        print(prev_header)
+
+                    except NoSuchElementException as e:
+                        break
 
             # Do again, but for historic    
 
-            driver.find_element_by_id('historic_root').click()
-            _ = WebDriverWait(driver, 30).until(
-                EC.element_to_be_clickable((By.ID, 'historic_lomc_root'))
-            )
-            driver.find_element_by_id('historic_lomc_root').click()
-            _ = WebDriverWait(driver, 30).until(
-                EC.element_to_be_clickable((By.ID, 'his_loma_root'))
-            )
-            driver.find_element_by_id('his_loma_root').click()
-            his_loma_root_elt = driver.find_element_by_css_selector('div[aria-labelledby="his_loma_root"]')
-            _ = WebDriverWait(driver, 30).until(
-                EC.visibility_of(his_loma_root_elt)
-            )
-            prev_header = ''
+            table_loaded = True
+            try:
+                driver.find_element_by_id('historic_root').click()
+                _ = WebDriverWait(driver, 120).until(
+                    EC.element_to_be_clickable((By.ID, 'historic_lomc_root'))
+                )
+                driver.find_element_by_id('historic_lomc_root').click()
+                _ = WebDriverWait(driver, 120).until(
+                    EC.element_to_be_clickable((By.ID, 'his_loma_root'))
+                )
+                driver.find_element_by_id('his_loma_root').click()
+                his_loma_root_elt = driver.find_element_by_css_selector('div[aria-labelledby="his_loma_root"]')
+                _ = WebDriverWait(driver, 120).until(
+                    EC.visibility_of(his_loma_root_elt)
+                )
+            except Exception as e:
+                table_loaded = False
+            
+            if table_loaded:
+                prev_header = ''
 
-            print(his_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML'))
+                print(his_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML'))
 
-            while True:
-                his_loma_list_elt = his_loma_root_elt.find_element_by_id('his_loma_list')
-                his_loma_rows = his_loma_list_elt.find_elements_by_css_selector('tr[role="row"]')
+                while True:
+                    his_loma_list_elt = his_loma_root_elt.find_element_by_id('his_loma_list')
+                    his_loma_rows = his_loma_list_elt.find_elements_by_css_selector('tr[role="row"]')
 
-                # Loop through all rows in table
-                for row in his_loma_rows:
-                    entries = row.find_elements_by_css_selector('td')
-                    file_id = entries[0].get_attribute('innerHTML')
-                    file_date = entries[1].get_attribute('innerHTML')
-                    file_link = entries[2].find_element_by_css_selector('a').get_attribute('href')
+                    # Loop through all rows in table
+                    for row in his_loma_rows:
+                        entries = row.find_elements_by_css_selector('td')
+                        file_id = entries[0].get_attribute('innerHTML')
+                        file_date = entries[1].get_attribute('innerHTML')
+                        file_link = entries[2].find_element_by_css_selector('a').get_attribute('href')
 
-                    act_link = 'https://map1.msc.fema.gov/data' + file_link[file_link.find('filepath') + 9 : file_link.find('.pdf') + 4]
-                    # print(file_id, file_date, file_link)
+                        act_link = 'https://map1.msc.fema.gov/data' + file_link[file_link.find('filepath') + 9 : file_link.find('.pdf') + 4]
+                        # print(file_id, file_date, file_link)
 
-                    # Download and write to csv
-                    if int(file_date[-4:]) >= 2000:
-                        urlretrieve(act_link, outp_path + '/' + file_id + '.pdf')
-                        outp.writerow([file_id, file_date])
+                        # Download and write to csv
+                        if int(file_date[-4:]) >= 2000:
+                            urlretrieve(act_link, outp_path + '/' + file_id + '.pdf')
+                            outp.writerow([file_id, file_date])
 
-                try:
-                    # Click next to go to next page
-                    next_button = his_loma_root_elt.find_element_by_css_selector('.next:not(.disabled)')
-                    next_button.click()
-                    _ = WebDriverWait(driver, 30).until(
-                        lambda dr: his_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML') != prev_header
-                    )
-                    prev_header = his_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML')
-                    print(prev_header)
+                    try:
+                        # Click next to go to next page
+                        next_button = his_loma_root_elt.find_element_by_css_selector('.next:not(.disabled)')
+                        next_button.click()
+                        _ = WebDriverWait(driver, 120).until(
+                            lambda dr: his_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML') != prev_header
+                        )
+                        prev_header = his_loma_root_elt.find_element_by_class_name('dataTables_info').get_attribute('innerHTML')
+                        print(prev_header)
 
-                except NoSuchElementException as e:
-                    break
+                    except NoSuchElementException as e:
+                        break
 
 finally:
     driver.quit()
